e301e71f1d2b081c8799e90d6ae52e91f2645b53
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-12-10 21:35:49 +0530

    Basic file setup

---
+++ Backend/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+function connectToDb() {
+  mongoose
+    .connect(
+      process.env.DB_CONNECT,
+      { useNewUrlParser: true },
+      console.log("Connect to DB")
+    )
+    .catch((err) => console.log(err));
+}
+
+module.exports = connectToDb;
